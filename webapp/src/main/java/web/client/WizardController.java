7d7a032f517cbd6d5f4a9f9c7b8578285607612d
andrew hinton <hintonandrew510@gmail.com>
2018-12-08 09:17:42 -0500

    add

---
+++ webapp/src/main/java/web/client/WizardController.java
@@ -0,0 +1,12 @@
+package web.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller    // This means that this class is a Controller
+public class WizardController {
+	@GetMapping(path = "/wizard") 
+	public String wizard() {
+		return "wizard";
+	}
+}
