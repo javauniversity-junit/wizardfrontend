159bf53c2381b4195bb8d96ec9510422ab207273
hintonandrew510 <hintonandrew510@gmail.com>
2018-12-21 17:53:07 -0500

    add

--- sql/backup.sh
+++ sql/backup.sh
@@ -7,8 +7,8 @@ logfile="$backupfolder/"backup_log_"$(date +'%Y_%m')".txt
 echo "mysqldump started at $(date +'%d-%m-%Y %H:%M:%S')" >> "$logfile"
 mysqldump --user=root --password=password --default-character-set=utf8 license | gzip > "$fullpathbackupfile"
 echo "mysqldump finished at $(date +'%d-%m-%Y %H:%M:%S')" >> "$logfile"
-chown myuser "$fullpathbackupfile"
-chown myuser "$logfile"
+chown wizard "$fullpathbackupfile"
+chown wizard "$logfile"
 echo "file permission changed" >> "$logfile"
 find "$backupfolder" -name db_backup_* -mtime +8 -exec rm {} \;
 echo "old files deleted" >> "$logfile"
