dd7d61ce79ac917d01ee3035d091b6fe0ef6c917
hintonandrew510 <hintonandrew510@gmail.com>
2018-12-26 19:04:47 +0000

    add

--- webmarket/src/main/java/com/scr/market/WebmarketApplication.java
+++ webmarket/src/main/java/com/scr/market/WebmarketApplication.java
@@ -3,10 +3,22 @@ package com.scr.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class WebmarketApplication {
+public class WebmarketApplication  extends SpringBootServletInitializer {
+     private static final Logger LOGGER = Logger.getLogger(WebmarketApplication.class.getName());
+
+   @Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebmarketApplication.class);
+}
+
 
 	public static void main(String[] args) {
+	    LOGGER.info("main  start");
 		SpringApplication.run(WebmarketApplication.class, args);
 	}
 
