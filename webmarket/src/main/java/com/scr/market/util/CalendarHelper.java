76b1288d7ef1ee1aed72242806265a03ac883019
hintonandrew510 <hintonandrew510@gmail.com>
2019-01-02 14:53:16 -0800

    Update CalendarHelper.java

--- webmarket/src/main/java/com/scr/market/util/CalendarHelper.java
+++ webmarket/src/main/java/com/scr/market/util/CalendarHelper.java
@@ -1,24 +1,43 @@
 package com.scr.market.util;
 import java.sql.Timestamp;
-import java.util.Calendar;
+import java.util.*;
+import java.text.*;
 
 public class CalendarHelper 
 {
 
-     public static boolean hasNotExpired(Timestamp startTime, Timestamp endTime) {
+     public static boolean hasExpired(Timestamp startTime, Timestamp endTime) {
+          SimpleDateFormat dateFormat = new SimpleDateFormat(
+            "yyyy-MM-dd hh:mm:ss");
        
           Calendar start = Calendar.getInstance();
           start.setTimeInMillis( startTime.getTime() );
           
+          System.out.println(" cal start "  + dateFormat.format(start.getTime()));
+          
           Calendar end = Calendar.getInstance();
-          start.setTimeInMillis( endTime.getTime() );
+          end.setTimeInMillis( endTime.getTime() );
+           System.out.println(" cal end "  +dateFormat.format(end.getTime()));
           
           Calendar currentTime = Calendar.getInstance();
           
-          boolean expired = currentTime.after(startTime) && currentTime.before(endTime);
+          
+          
+           boolean isCurrentTimeAfterStartTime = currentTime.after(start);
+          boolean isCurrentTimeBeforEndTime  = currentTime.before(end);
+          
+          System.out.println("currentTime.after(start) " + isCurrentTimeAfterStartTime);
+          System.out.println("currentTime.before(end) " + isCurrentTimeBeforEndTime);
+          
+          boolean expired = true;
+          if (isCurrentTimeAfterStartTime &&  isCurrentTimeBeforEndTime) {
+              return false;
+          }
    
           
         return expired; 
+   
+         
      }
 
 
