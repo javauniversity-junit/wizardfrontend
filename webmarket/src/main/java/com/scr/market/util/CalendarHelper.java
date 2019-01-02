f66a03188646463a460aeeeabf8862e7f4cd977a
hintonandrew510 <hintonandrew510@gmail.com>
2019-01-02 12:43:46 -0800

    Update CalendarHelper.java

--- webmarket/src/main/java/com/scr/market/util/CalendarHelper.java
+++ webmarket/src/main/java/com/scr/market/util/CalendarHelper.java
@@ -1,7 +1,14 @@
 package com.scr.market.util;
+import java.sql.Timestamp;
+import java.util.Calendar
+
 public class CalendarHelper 
 {
 
+     public boolean hasExpired() {
+       
+        return false; 
+     }
 
 
 
