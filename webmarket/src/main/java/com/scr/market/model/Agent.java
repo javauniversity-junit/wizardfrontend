d4fa92466df18450637c913fdfeeab9203da1c3d
hintonandrew510 <hintonandrew510@gmail.com>
2019-01-03 15:19:57 -0800

    Update Agent.java

--- webmarket/src/main/java/com/scr/market/model/Agent.java
+++ webmarket/src/main/java/com/scr/market/model/Agent.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 
 public class Agent {
 	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer agentid;
 	
 	private Integer contactId;
