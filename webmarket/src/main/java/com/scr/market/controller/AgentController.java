9f4ec699cba54302fa5414b15ea01fea47d84400
hintonandrew510 <hintonandrew510@gmail.com>
2019-01-03 11:21:31 -0800

    Update AgentController.java

--- webmarket/src/main/java/com/scr/market/controller/AgentController.java
+++ webmarket/src/main/java/com/scr/market/controller/AgentController.java
@@ -40,8 +40,8 @@ private static final Logger mLog = Logger.getLogger(AgentController.class.getNam
 		// @RequestParam means it is a parameter from the GET or POST request
 
 		Agent agent = new Agent();
-		agent.setAddress(email-input);
-		agent.SetPassword(password-input);
+		agent.setAddress(email);
+		agent.SetPassword(password);
 		agent.setContactId(contact.getContactId());
 
 		agentRepository.save(agent);
