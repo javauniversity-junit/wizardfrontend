b4a44c6940edb896e149e7f49746184b36078e0b
hintonandrew510 <hintonandrew510@gmail.com>
2019-01-08 17:00:26 -0500

    Update deploy.sh

--- webmarket/deploy.sh
+++ webmarket/deploy.sh
@@ -4,4 +4,4 @@ rm -rf  /home/wizard/source/wizardfrontend
 git clone https://github.com/hintonandrew510/wizardfrontend.git
 cd /home/wizard/source/wizardfrontend/webmarket
 mvn clean install
-cp ${current.project.path}/webmarket/target/*.war /home/user/tomcat8/webapps
+cp ${current.project.path}/webmarket/target/*.war /usr/share/tomcat/webapps
