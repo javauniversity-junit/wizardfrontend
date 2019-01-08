c8fd584265a2505c61405337dd7d859aa0114efc
hintonandrew510 <hintonandrew510@gmail.com>
2019-01-08 16:55:56 -0500

    add

--- webmarket/deploy.sh
+++ webmarket/deploy.sh
@@ -1,1 +1,7 @@
 # get source from github
+cd /home/wizard/source
+rm -rf  /home/wizard/source/wizardfrontend
+git clone https://github.com/hintonandrew510/wizardfrontend.git
+cd /home/wizard/source/wizardfrontend/webmarket
+mvn clean install
+cp ${current.project.path}/webmarket/target/*.war /home/user/tomcat8/webapps
