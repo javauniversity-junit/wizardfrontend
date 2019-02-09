7e2a2c074cd4d2c6555f07296f6a87793f0c2f6a
DESKTOP-KI5T3AK\tinas <tinaschool2014@gmail.com>
2019-02-09 10:00:34 -0500

    Save Page
    Save Page

--- webapp/src/main/java/web/page/marketplacecompetitionpage/MarketPlaceCompetitionPageModel.java
+++ webapp/src/main/java/web/page/marketplacecompetitionpage/MarketPlaceCompetitionPageModel.java
@@ -2,6 +2,22 @@ package web.page.marketplacecompetitionpage;
 
 public class MarketPlaceCompetitionPageModel {
 	
+	public MarketPlaceCompetitionPageModel(String firstTierBusiness, String secondTierBusiness,
+			String thirdTierBusiness, String fourthTierBusiness, String competitionA, String competitionB,
+			String competitionStrengthA, String competitionStrengthB, String competitionWeaknessA,
+			String competitionWeaknessB) {
+		super();
+		this.firstTierBusiness = firstTierBusiness;
+		this.secondTierBusiness = secondTierBusiness;
+		this.thirdTierBusiness = thirdTierBusiness;
+		this.fourthTierBusiness = fourthTierBusiness;
+		this.competitionA = competitionA;
+		this.competitionB = competitionB;
+		this.competitionStrengthA = competitionStrengthA;
+		this.competitionStrengthB = competitionStrengthB;
+		this.competitionWeaknessA = competitionWeaknessA;
+		this.competitionWeaknessB = competitionWeaknessB;
+	}
 	public String getFirstTierBusiness() {
 		return firstTierBusiness;
 	}
@@ -16,5 +32,63 @@ public class MarketPlaceCompetitionPageModel {
 	}
 	private String firstTierBusiness;
 	private String secondTierBusiness;
+	private String thirdTierBusiness;
+	private String fourthTierBusiness;
+	private String competitionA;
+	private String competitionB;
+	private String competitionStrengthA;
+	private String competitionStrengthB;
+	private String competitionWeaknessA;
+	private String competitionWeaknessB;
+	public String getThirdTierBusiness() {
+		return thirdTierBusiness;
+	}
+	public void setThirdTierBusiness(String thirdTierBusiness) {
+		this.thirdTierBusiness = thirdTierBusiness;
+	}
+	public String getFourthTierBusiness() {
+		return fourthTierBusiness;
+	}
+	public void setFourthTierBusiness(String fourthTierBusiness) {
+		this.fourthTierBusiness = fourthTierBusiness;
+	}
+	public String getCompetitionA() {
+		return competitionA;
+	}
+	public void setCompetitionA(String competitionA) {
+		this.competitionA = competitionA;
+	}
+	public String getCompetitionB() {
+		return competitionB;
+	}
+	public void setCompetitionB(String competitionB) {
+		this.competitionB = competitionB;
+	}
+	public String getCompetitionStrengthA() {
+		return competitionStrengthA;
+	}
+	public void setCompetitionStrengthA(String competitionStrengthA) {
+		this.competitionStrengthA = competitionStrengthA;
+	}
+	public String getCompetitionStrengthB() {
+		return competitionStrengthB;
+	}
+	public void setCompetitionStrengthB(String competitionStrengthB) {
+		this.competitionStrengthB = competitionStrengthB;
+	}
+	public String getCompetitionWeaknessA() {
+		return competitionWeaknessA;
+	}
+	public void setCompetitionWeaknessA(String competitionWeaknessA) {
+		this.competitionWeaknessA = competitionWeaknessA;
+	}
+	public String getCompetitionWeaknessB() {
+		return competitionWeaknessB;
+	}
+	public void setCompetitionWeaknessB(String competitionWeaknessB) {
+		this.competitionWeaknessB = competitionWeaknessB;
+	}
+	
+	
 
 }
