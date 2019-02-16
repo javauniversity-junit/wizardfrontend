c9078ddec068975bb82f084605da669e43112bf8
andrew hinton <hintonandrew510@gmail.com>
2019-02-16 16:35:11 -0500

    adsd
    aaa

--- webapp/src/main/resources/static/js/ClientObjectivesOnePageControl.js
+++ webapp/src/main/resources/static/js/ClientObjectivesOnePageControl.js
@@ -10,22 +10,19 @@ map.set('retainCurrentConsumersLabel', 'Retain/Recapture Consumers');
 
 map.set('increaseCustomerVisitsLabel', 'Increase Frequency of Customer Visits');
 map.set('increaseTrafficLeadCallsLabel', 'Grow New Traffic/Lead Calls');
-map.set('introduceNewDepartLabment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
-map.set('introduceNewDepartment', 'Introduce New Department/Products/Services');
+map.set('expandTargetConsumersLabel', 'Expand Target Consumers (by age, sex, geography)');
+map.set('changeConsumerAttitudesLabel', 'Change Consumer Attitudes');
+map.set('makePromotionalEventsStrongerLabel', 'Make Promotional Events Stronger');
+map.set('increaseDigitalMobileOnlineResponseLabel', 'Develop Digital/Mobile/Social Strategies');
+map.set('developDatabaseMarketingLabel', 'Develop Database Marketing (Email and Text)');
+map.set('initiateCauseMarketingProgramLabel', 'Initiate Cause Marketing Programs');
+map.set('developSpeciallyStagedEventLabel', 'Develop Specially Staged Events');
+map.set('maintainMarketDominanceLabel', 'Maintain Market Dominance');
+map.set('improveBusinessNameBrandLabel', 'Elevate Business Brand/Image');
+map.set('establishorReestablishBusinessImageLabel', 'Improve Reputation and Listing Management');
+map.set('createPentUpDemandLabel', 'Enhance Website (Mobile, Response, SEO, SEM)');
+map.set('increaseMarketShareLabel', 'Increase Market Share');
+map.set('otherLabel', 'Other:');
 
 
 function addLabel(key) {
