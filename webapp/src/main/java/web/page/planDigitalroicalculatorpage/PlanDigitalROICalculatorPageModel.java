4aedacd3387ae58bc95b47a70dec19ed6d435724
andrew hinton <hintonandrew510@gmail.com>
2019-03-30 10:49:25 -0400

    add

---
+++ webapp/src/main/java/web/page/planDigitalroicalculatorpage/PlanDigitalROICalculatorPageModel.java
@@ -0,0 +1,117 @@
+package web.page.planDigitalroicalculatorpage;
+
+public class PlanDigitalROICalculatorPageModel {
+	private String planAMonthlyDigitalInvestment;
+	private String planACostPerThousand;
+	private String planAClosingPctNumber;
+	private String planAClickThroughRate;
+	private String planAMonthlyClicks;
+	private String planACostPerClicks;
+	private String planASalesConversionPercentage;
+	private String planAMonthlyConversions;
+	private String planACostPerConversion;
+	private String planAProjectedGrossProfit;
+	private String planAMonthlyGrossProfit;
+	private String planAMonthlyROID;
+	private String planAMonthlyROIP;
+	
+	public PlanDigitalROICalculatorPageModel(String planAMonthlyDigitalInvestment, String planACostPerThousand,
+			String planAClosingPctNumber, String planAClickThroughRate, String planAMonthlyClicks,
+			String planACostPerClicks, String planASalesConversionPercentage, String planAMonthlyConversions,
+			String planACostPerConversion, String planAProjectedGrossProfit, String planAMonthlyGrossProfit,
+			String planAMonthlyROID, String planAMonthlyROIP) {
+		super();
+		this.planAMonthlyDigitalInvestment = planAMonthlyDigitalInvestment;
+		this.planACostPerThousand = planACostPerThousand;
+		this.planAClosingPctNumber = planAClosingPctNumber;
+		this.planAClickThroughRate = planAClickThroughRate;
+		this.planAMonthlyClicks = planAMonthlyClicks;
+		this.planACostPerClicks = planACostPerClicks;
+		this.planASalesConversionPercentage = planASalesConversionPercentage;
+		this.planAMonthlyConversions = planAMonthlyConversions;
+		this.planACostPerConversion = planACostPerConversion;
+		this.planAProjectedGrossProfit = planAProjectedGrossProfit;
+		this.planAMonthlyGrossProfit = planAMonthlyGrossProfit;
+		this.planAMonthlyROID = planAMonthlyROID;
+		this.planAMonthlyROIP = planAMonthlyROIP;
+	}
+	public String getPlanAMonthlyDigitalInvestment() {
+		return planAMonthlyDigitalInvestment;
+	}
+	public void setPlanAMonthlyDigitalInvestment(String planAMonthlyDigitalInvestment) {
+		this.planAMonthlyDigitalInvestment = planAMonthlyDigitalInvestment;
+	}
+	public String getPlanACostPerThousand() {
+		return planACostPerThousand;
+	}
+	public void setPlanACostPerThousand(String planACostPerThousand) {
+		this.planACostPerThousand = planACostPerThousand;
+	}
+	public String getPlanAClosingPctNumber() {
+		return planAClosingPctNumber;
+	}
+	public void setPlanAClosingPctNumber(String planAClosingPctNumber) {
+		this.planAClosingPctNumber = planAClosingPctNumber;
+	}
+	public String getPlanAClickThroughRate() {
+		return planAClickThroughRate;
+	}
+	public void setPlanAClickThroughRate(String planAClickThroughRate) {
+		this.planAClickThroughRate = planAClickThroughRate;
+	}
+	public String getPlanAMonthlyClicks() {
+		return planAMonthlyClicks;
+	}
+	public void setPlanAMonthlyClicks(String planAMonthlyClicks) {
+		this.planAMonthlyClicks = planAMonthlyClicks;
+	}
+	public String getPlanACostPerClicks() {
+		return planACostPerClicks;
+	}
+	public void setPlanACostPerClicks(String planACostPerClicks) {
+		this.planACostPerClicks = planACostPerClicks;
+	}
+	public String getPlanASalesConversionPercentage() {
+		return planASalesConversionPercentage;
+	}
+	public void setPlanASalesConversionPercentage(String planASalesConversionPercentage) {
+		this.planASalesConversionPercentage = planASalesConversionPercentage;
+	}
+	public String getPlanAMonthlyConversions() {
+		return planAMonthlyConversions;
+	}
+	public void setPlanAMonthlyConversions(String planAMonthlyConversions) {
+		this.planAMonthlyConversions = planAMonthlyConversions;
+	}
+	public String getPlanACostPerConversion() {
+		return planACostPerConversion;
+	}
+	public void setPlanACostPerConversion(String planACostPerConversion) {
+		this.planACostPerConversion = planACostPerConversion;
+	}
+	public String getPlanAProjectedGrossProfit() {
+		return planAProjectedGrossProfit;
+	}
+	public void setPlanAProjectedGrossProfit(String planAProjectedGrossProfit) {
+		this.planAProjectedGrossProfit = planAProjectedGrossProfit;
+	}
+	public String getPlanAMonthlyGrossProfit() {
+		return planAMonthlyGrossProfit;
+	}
+	public void setPlanAMonthlyGrossProfit(String planAMonthlyGrossProfit) {
+		this.planAMonthlyGrossProfit = planAMonthlyGrossProfit;
+	}
+	public String getPlanAMonthlyROID() {
+		return planAMonthlyROID;
+	}
+	public void setPlanAMonthlyROID(String planAMonthlyROID) {
+		this.planAMonthlyROID = planAMonthlyROID;
+	}
+	public String getPlanAMonthlyROIP() {
+		return planAMonthlyROIP;
+	}
+	public void setPlanAMonthlyROIP(String planAMonthlyROIP) {
+		this.planAMonthlyROIP = planAMonthlyROIP;
+	}
+	
+}
