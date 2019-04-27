82a857ef34f548f2815c8ccc42f2d066cf74bbec
hintonandrew510 <hintonandrew510@gmail.com>
2019-04-27 10:50:22 -0400

    Update ConfidentialClientEvaluationOnePage.js

--- webapp/src/main/resources/static/js/ConfidentialClientEvaluationOnePage.js
+++ webapp/src/main/resources/static/js/ConfidentialClientEvaluationOnePage.js
@@ -1,4 +1,4 @@
-function calculate()
+function calculateOne()
 {
 	var lastYearValue1 = document.getElementById("lastYearValue1").value;
 	var lastYearValue2 = document.getElementById("lastYearValue2").value;
@@ -75,7 +75,105 @@ function calculate()
 	if (lastYearValue6 != null) {
 		lastYearValue6 = lastYearValue6.substring(1);
 		lastYearValue6 = removeComma(lastYearValue6);
-		llastYearValue6Cal = Number(lastYearValue6);
+		lastYearValue6Cal = Number(lastYearValue6);
+	}
+	
+	
+	
+
+
+	
+	var totalMediaInvestment = lastYearValue1Cal + lastYearValue2Cal + lastYearValue3Cal + lastYearValue4Cal
+	                  + lastYearValue5Cal + lastYearValue6Cal;
+	
+	document.getElementById("totalMediaInvestment").value =  totalMediaInvestment;
+	//document.getElementById("averageCustomerValue").value = e_averageCustomerValue;//e
+	//document.getElementById("lifetimeValuePerCustomer").value = g_lifetimeValuePerCustomer;
+	//document.getElementById("prospectsNeededToBreakEven").value = i_prospectsNeededToBreakEven;
+}
+
+
+
+
+
+function calculateTwo()
+{
+	var proposedValue1 = document.getElementById("proposedValue1").value;
+	var proposedValue2 = document.getElementById("proposedValue2").value;
+	var proposedValue3 = document.getElementById("proposedValue3").value;
+	
+	var pctTAIGrossSalesLastYear = document.getElementById("pctTAIGrossSalesLastYear").value;
+	var totalMediaInvestment = document.getElementById("totalMediaInvestment").value;
+	
+	//TotalMediaInvestment / pctTAIGrossSalesLastYear = GrossSalesLastyear
+	
+	var proposedValue4 = document.getElementById("proposedValue4").value;
+	var proposedValue5= document.getElementById("proposedValue5").value;
+	var proposedValue6= document.getElementById("proposedValue6").value;
+	var proposedValue1Cal =0;
+	var proposedValue2Cal =0;
+	var proposedValue3Cal =0;
+	var proposedValue4Cal =0;
+	var proposedValue5Cal =0;
+	var proposedValue6Cal =0;
+	
+	var pctTAIGrossSalesLastYearCal = 0;
+	var totalMediaInvestmentCal = 0;
+	
+	
+	if (totalMediaInvestment != null) {
+		totalMediaInvestment = totalMediaInvestment.substring(1);
+		totalMediaInvestment = removeComma(totalMediaInvestment);
+		totalMediaInvestmentCal = Number(totalMediaInvestment);
+	}	
+	
+	
+	if (pctTAIGrossSalesLastYear != null) {
+		
+	
+		pctTAIGrossSalesLastYearCal = Number(pctTAIGrossSalesLastYear);
+	}
+	
+	if (totalMediaInvestmentCal > 0 && pctTAIGrossSalesLastYearCal > 0) {
+		var percentage = 100/pctTAIGrossSalesLastYearCal;
+		var total = totalMediaInvestmentCal/ percentage;
+		total = Math.round(total);
+		document.getElementById("grossSalesLastyear").value =  total;
+		
+	}
+	
+	
+	
+	if (proposedValue1 != null) {
+		proposedValue1 = proposedValue1.substring(1);
+		proposedValue1 = removeComma(proposedValue1);
+		proposedValue1Cal = Number(proposedValue1);
+	}
+	if (proposedValue2 != null) {
+		proposedValue2 = proposedValue2.substring(1);
+		proposedValue2 = removeComma(proposedValue2);
+		proposedValue2Cal = Number(proposedValue2);
+	}
+	
+	if proposedValue3 != null) {
+		proposedValue3 = proposedValue3.substring(1);
+		proposedValue3 = removeComma(proposedValue3);
+		proposedValue3Cal = Number(proposedValue3);
+	}
+	if (proposedValue4 != null) {
+		proposedValue4 = proposedValue4.substring(1);
+		proposedValue4 = removeComma(proposedValue4);
+		proposedValue4Cal = Number(lastYearValue4);
+	}
+	if (proposedValue5 != null) {
+		proposedValue5 = proposedValue5.substring(1);
+		proposedValue5 = removeComma(proposedValue5);
+		proposedValue5Cal = Number(proposedValue5);
+	}
+	if (proposedValue6 != null) {
+		proposedValue6 = proposedValue6.substring(1);
+		lastYearValue6 = removeComma(proposedValue6);
+		proposedValue6Cal = Number(proposedValue6);
 	}
 	
 	
@@ -94,7 +192,8 @@ function calculate()
 
 
 function totalRow() {
-	calculate();
+	calculateOne();
+	calculateTwo();
 	//document.getElementById("totalRow").value = total;
 }
 
