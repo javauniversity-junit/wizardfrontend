c9ae4f09018327738cbc5b5dced27fd8b159d1cc
DESKTOP-KI5T3AK\tinas <tinaschool2014@gmail.com>
2019-04-29 00:57:37 -0400

    PlanABEPPage

--- webapp/src/main/resources/static/js/planBEPCalculate.js
+++ webapp/src/main/resources/static/js/planBEPCalculate.js
@@ -4,21 +4,20 @@ function calculate()
 	var planAGrossMargin = document.getElementById("planAGrossMargin").value; // b
 	var planAClosingPct = document.getElementById("planAClosingPct").value; // c
 	var planAProspectValue = planAAverageSale * planAGrossMargin * planAClosingPct; // d = abc
-	document.getElementById("planAProspectValue").value =  planAProspectValue;
+
 
 	var planAInvestment = document.getElementById("planAAverageSale").value; // e
-	
-	var planAProspectsNeeded = planAInvestment / planAProspectValue; //f
-	document.getElementById("planAProspectsNeeded").value =  planAProspectsNeeded;
-	
+	var planAProspectsNeeded = planAInvestment / planAProspectValue; // f = e/d
 	var planAProspectSalesNeeded = planAProspectsNeeded * planAClosingPct; // g = fc
-	document.gplanAProspectsNeededetElementById("planAProspectSalesNeeded").value =  planAProspectSalesNeeded;
-	
 	var planAGrossProfitOnSales = planAProspectSalesNeeded * planAAverageSale; // h = ga
-	document.gplanAProspectsNeededetElementById("planAGrossProfitOnSales").value =  planAGrossProfitOnSales;
-	
 	var planAMonths = document.gplanAProspectsNeededetElementById("planAMonths").value; // i
 	var planAAdditionalGrossSales = planAGrossProfitOnSales * planAMonths; // j = hi
+	
+	document.getElementById("planAProspectValue").value =  planAProspectValue;
+	document.getElementById("planAProspectsNeeded").value =  planAProspectsNeeded;
+	document.getElementById("planAProspectSalesNeeded").value =  planAProspectSalesNeeded;	
+	document.getElementById("planAGrossProfitOnSales").value =  planAGrossProfitOnSales;
+	document.getElementById("planAAdditionalGrossSales").value =  planAAdditionalGrossSales;
 }
 
 function totalRow() {
