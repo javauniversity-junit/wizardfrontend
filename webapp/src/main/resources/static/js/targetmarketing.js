075a175eeb6d6f1964ee507b3393a48891af76d3
DESKTOP-KI5T3AK\tinas <tinaschool2014@gmail.com>
2019-07-12 23:04:14 -0400

    changes

--- webapp/src/main/resources/static/js/targetmarketing.js
+++ webapp/src/main/resources/static/js/targetmarketing.js
@@ -1,18 +1,37 @@
-function calculateMen () {
+function calculateWomen () {
 
 	//clear
 	console.clear();
 	
 	var pctMen = document.getElementById("pctMen").value;
     var pctWomen = 100 - pctMen;
-    document.getElementById("pctWomen").value =  = pctWomen;
+    document.getElementById("pctWomen").value = pctWomen;
 }
 
-function calculateWomen () {
+function calculateMen () {
 
 	//clear
 	console.clear();
 	var pctWomen = document.getElementById("pctWomen").value;
     var pctMen = 100 - pctWomen;
-    document.getElementById("pctMen").value =  = pctMen;
+    document.getElementById("pctMen").value = pctMen;
+}
+
+function calculateHave (haveParm, wantParm) {
+
+	//clear
+	console.clear();
+	
+	var wantvalue = document.getElementById(wantParm).value;
+    var haveValue = 100 - wantvalue;
+    document.getElementById(haveParm).value = haveValue;
+}
+
+function calculateWant (haveParm, wantParm) {
+	//clear
+	console.clear();
+	
+	var haveValue = document.getElementById(haveParm).value;
+    var wantValue = 100 - haveValue;
+    document.getElementById(wantParm).value = wantValue;
 }
