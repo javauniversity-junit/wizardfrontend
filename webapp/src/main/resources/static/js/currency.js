1be03f72e29307311175c8ec7a8b31962a2f939d
DESKTOP-KI5T3AK\tinas <tinaschool2014@gmail.com>
2019-08-31 17:33:25 -0400

    c

--- webapp/src/main/resources/static/js/currency.js
+++ webapp/src/main/resources/static/js/currency.js
@@ -157,4 +157,80 @@ function CurrencyFormatted(amount) {
 }
 
 
+// tinas
+$("input[data-type='numberwithcomma']").on({
+    keyup: function() {
+    	addCommas($(this));
+    	//return "5,000";
+    } ,
+    blur: function() { 
+    	addCommas($(this), "blur");
+    } 
+});
+
+function addCommas(input, blur) {
+	  // appends $ to value, validates decimal side
+	  // and puts cursor back in right position.
+	  
+	  // get input value
+	  var input_val = input.val();
+	 
+	  
+	  // don't validate empty input
+	  if (input_val === "") { 
+		 input_val = "0"; 
+	   }
+		
+	 //remove period
+	  input_val = input_val.replace('.', "");
+	  
+	  // original length
+	  var original_len = input_val.length;
+
+	  // initial caret position 
+	  var caret_pos = input.prop("selectionStart");
+	    
+	  // check for decimal
+	  if (input_val.indexOf(".") >= 0) {
+
+	    // get position of first decimal
+	    // this prevents multiple decimals from
+	    // being entered
+	    var decimal_pos = input_val.indexOf(".");
+
+	    // split number by decimal point
+	    var left_side = input_val.substring(0, decimal_pos);
+	    var right_side = input_val.substring(decimal_pos);
+
+	    // add commas to left side of number
+	    left_side = formatNumber(left_side);
+
+	    // validate right side
+	    right_side = formatNumber(right_side);
+	    
+	    // On blur make sure 2 numbers after decimal
+	    if (blur === "blur") {
+	     //aah right_side += "00";
+	     
+	    }
+	    
+	    // Limit decimal to only 2 digits
+	    right_side = right_side.substring(0, 2);
+
+	    // join number by .
+	    input_val = left_side + "." + right_side;
+
+	  } else {
+	    // no decimal entered
+	    // add commas to number
+	    // remove all non-digits
+	    input_val = formatNumber(input_val);
+	    input_val = input_val;
+	    
+	    // final formatting
+	    if (blur === "blur") {
+	      //aah input_val += ".00";
+	    }
+	  }
+}
 
