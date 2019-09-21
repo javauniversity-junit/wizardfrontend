43b739826ff0d26e268f7343c1ed495cc6076af9
hintonandrew510 <hintonandrew510@gmail.com>
2019-09-21 17:33:09 -0400

    Update ChartBuilder.java

--- webapp/src/main/java/web/page/ChartBuilder.java
+++ webapp/src/main/java/web/page/ChartBuilder.java
@@ -7,6 +7,7 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Logger;
 
 import web.page.confidentialclientevaluationnonepage.ConfidentialClientEvaluationOnePageModel;
 
