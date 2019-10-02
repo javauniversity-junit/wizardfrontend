483a6fefd45434c307867e4d4a42e992bab3789a
andrew hinton <hintonandrew510@gmail.com>
2019-10-02 18:29:50 -0400

    add

--- webapp/src/main/java/web/page/ChartBuilder.java
+++ webapp/src/main/java/web/page/ChartBuilder.java
@@ -46,6 +46,112 @@ private static final Logger mLog = Logger.getLogger(ChartBuilder.class.getName()
 		}
 		
 	} 
+	if (model.getLastYearLabel3() != null && model.getLastYearValue3() != null) {
+		//amount
+		try {
+			
+		int amount = 	parse(model.getLastYearValue3());
+		PieChart pieChart = new PieChart(model.getLastYearLabel3(),amount);
+		mLog.info(" add to piechart label [" + model.getLastYearLabel3() + "]");
+		mLog.info(" add to piechart label [" + amount + "]");
+		pieCharts.add(pieChart);	
+			
+		} catch (Exception ex) {
+			mLog.warning("Could not parse amount [" + model.getLastYearValue3() + "]"); 
+		}
+		
+	}
+	if (model.getLastYearLabel4() != null && model.getLastYearValue4() != null) {
+		//amount
+		try {
+			
+		int amount = 	parse(model.getLastYearValue4());
+		PieChart pieChart = new PieChart(model.getLastYearLabel4(),amount);
+		mLog.info(" add to piechart label [" + model.getLastYearLabel4() + "]");
+		mLog.info(" add to piechart label [" + amount + "]");
+		pieCharts.add(pieChart);	
+			
+		} catch (Exception ex) {
+			mLog.warning("Could not parse amount [" + model.getLastYearValue4() + "]"); 
+		}
+		
+	}
+	if (model.getLastYearLabel5() != null && model.getLastYearValue5() != null) {
+		//amount
+		try {
+			
+		int amount = 	parse(model.getLastYearValue5());
+		PieChart pieChart = new PieChart(model.getLastYearLabel5(),amount);
+		mLog.info(" add to piechart label [" + model.getLastYearLabel5() + "]");
+		mLog.info(" add to piechart label [" + amount + "]");
+		pieCharts.add(pieChart);	
+			
+		} catch (Exception ex) {
+			mLog.warning("Could not parse amount [" + model.getLastYearValue5() + "]"); 
+		}
+		
+	}
+	if (model.getLastYearLabel6() != null && model.getLastYearValue6() != null) {
+		//amount
+		try {
+			
+		int amount = 	parse(model.getLastYearValue6());
+		PieChart pieChart = new PieChart(model.getLastYearLabel6(),amount);
+		mLog.info(" add to piechart label [" + model.getLastYearLabel6() + "]");
+		mLog.info(" add to piechart label [" + amount + "]");
+		pieCharts.add(pieChart);	
+			
+		} catch (Exception ex) {
+			mLog.warning("Could not parse amount [" + model.getLastYearValue6() + "]"); 
+		}
+		
+	}
+	if (model.getLastYearLabel7() != null && model.getLastYearValue7() != null) {
+		//amount
+		try {
+			
+		int amount = 	parse(model.getLastYearValue7());
+		PieChart pieChart = new PieChart(model.getLastYearLabel7(),amount);
+		mLog.info(" add to piechart label [" + model.getLastYearLabel7() + "]");
+		mLog.info(" add to piechart label [" + amount + "]");
+		pieCharts.add(pieChart);	
+			
+		} catch (Exception ex) {
+			mLog.warning("Could not parse amount [" + model.getLastYearValue7() + "]"); 
+		}
+		
+	}
+	if (model.getLastYearLabel8() != null && model.getLastYearValue8() != null) {
+		//amount
+		try {
+			
+		int amount = 	parse(model.getLastYearValue8());
+		PieChart pieChart = new PieChart(model.getLastYearLabel8(),amount);
+		mLog.info(" add to piechart label [" + model.getLastYearLabel8() + "]");
+		mLog.info(" add to piechart label [" + amount + "]");
+		pieCharts.add(pieChart);	
+			
+		} catch (Exception ex) {
+			mLog.warning("Could not parse amount [" + model.getLastYearValue8() + "]"); 
+		}
+		
+	}
+	if (model.getLastYearLabel9() != null && model.getLastYearValue9() != null) {
+		//amount
+		try {
+			
+		int amount = 	parse(model.getLastYearValue9());
+		PieChart pieChart = new PieChart(model.getLastYearLabel9(),amount);
+		mLog.info(" add to piechart label [" + model.getLastYearLabel9() + "]");
+		mLog.info(" add to piechart label [" + amount + "]");
+		pieCharts.add(pieChart);	
+			
+		} catch (Exception ex) {
+			mLog.warning("Could not parse amount [" + model.getLastYearValue9() + "]"); 
+		}
+		
+	}
+	
 	
 	return pieCharts;
  }
