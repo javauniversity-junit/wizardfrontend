a56766ca50ce4a4bfdf3b372c43e376ccdd04d91
hintonandrew510 <hintonandrew510@gmail.com>
2019-10-07 13:07:06 -0400

    Create MediaChart.java

---
+++ webapp/src/main/java/web/page/planamedipage/MediaChart.java
@@ -0,0 +1,29 @@
+public class MediaChart{
+     private int jan;
+      private int feb;
+       private int mar;
+      private int apr;
+           private int may;
+      private int jun;
+       private int jul;
+      private int aug;
+           private int sep;
+      private int oct;
+       private int nov;
+      private int dec;
+           private String janColor;
+      private String febColor;
+       private String marColor;
+      private String aprColor;
+           private String mayColor;
+      private String junColor;
+       private String julColor;
+      private String augColor;
+           private String sepColor;
+      private String octColor;
+       private String novColor;
+      private String decColor;
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}
