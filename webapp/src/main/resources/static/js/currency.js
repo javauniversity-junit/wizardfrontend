bfe54949e753ba4f0999b07a65b1afa9f34b8e76
DESKTOP-KI5T3AK\tinas <tinaschool2014@gmail.com>
2019-10-09 01:14:18 -0400

    l

--- webapp/src/main/resources/static/js/currency.js
+++ webapp/src/main/resources/static/js/currency.js
@@ -10,6 +10,15 @@ $("input[data-type='currency']").on({
     }
 });
 
+$("input[data-type='currencyWithCents']").on({
+    keyup: function() {
+      formatCurrencyWithCents($(this));
+    },
+    blur: function() { 
+    	formatCurrencyWithCents($(this), "blur");
+    }
+});
+
 function convertIntToNumber(intValue) {
 	if (intValue == null || intValue =="" ) {
 		return 0;
@@ -135,6 +144,84 @@ function formatCurrency(input, blur) {
   totalRow();
 }
   
+
+function formatCurrencyWithCents(input, blur) {
+	  // appends $ to value, validates decimal side
+	  // and puts cursor back in right position.
+	  
+	  // get input value
+	  var input_val = input.val();
+	 
+	  
+	  // don't validate empty input
+	  if (input_val === "") { 
+		 input_val = "0"; 
+	   }
+		
+	 //remove period
+	 // tinas input_val = input_val.replace('.', "");
+	  
+	  // original length
+	  var original_len = input_val.length;
+
+	  // initial caret position 
+	  var caret_pos = input.prop("selectionStart");
+	    
+	  // check for decimal
+	  if (input_val.indexOf(".") >= 0) {
+
+	    // get position of first decimal
+	    // this prevents multiple decimals from
+	    // being entered
+	    var decimal_pos = input_val.indexOf(".");
+
+	    // split number by decimal point
+	    var left_side = input_val.substring(0, decimal_pos);
+	    var right_side = input_val.substring(decimal_pos);
+
+	    // add commas to left side of number
+	    left_side = formatNumber(left_side);
+
+	    // validate right side
+	    right_side = formatNumber(right_side);
+	    
+	    // On blur make sure 2 numbers after decimal
+	    if (blur === "blur") {
+	     //aah right_side += "00";
+	     
+	    }
+	    
+	    // Limit decimal to only 2 digits
+	    right_side = right_side.substring(0, 2);
+
+	    // join number by .
+	    input_val = "$" + left_side + "." + right_side;
+
+	  } else {
+	    // no decimal entered
+	    // add commas to number
+	    // remove all non-digits
+	    input_val = formatNumber(input_val);
+	    input_val = "$" + input_val;
+	    
+	    // final formatting
+	    if (blur === "blur") {
+	      //aah input_val += ".00";
+	    }
+	  }
+	  
+	  // send updated string to input
+	  input.val(input_val);
+
+	  // put caret back in the right position
+	  var updated_len = input_val.length;
+	  caret_pos = updated_len - original_len + caret_pos;
+	  input[0].setSelectionRange(caret_pos, caret_pos);
+	  totalRow();
+	}
+
+
+
 function CurrencyFormatted(amount) {
 	var i = parseFloat(amount);
 	if(isNaN(i)) { i = 0.00; }
