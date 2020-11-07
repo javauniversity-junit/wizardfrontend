ac441bf390a991356207f58f526f4053caea9454
andrew hinton <hintonandrew510@gmail.com>
2020-11-07 18:02:46 -0500

    add

--- webapp/src/main/java/web/google/slide/GoogleSlideController.java
+++ webapp/src/main/java/web/google/slide/GoogleSlideController.java
@@ -1,41 +1,121 @@
 package web.google.slide;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.slides.v1.Slides;
+import com.google.api.services.slides.v1.SlidesScopes;
+import com.google.api.services.slides.v1.model.Page;
+import com.google.api.services.slides.v1.model.Presentation;
 
 @Controller // This means that this class is a Controller
 
 public class GoogleSlideController {
 	private static final Logger mLog = Logger.getLogger(GoogleSlideController.class.getName());
 
+	private static final String APPLICATION_NAME = "Google Slides API Java Quickstart";
+	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+	/**
+	 * Global instance of the scopes required by this quickstart. If modifying these
+	 * scopes, delete your previously saved tokens/ folder.
+	 */
+	private static final List<String> SCOPES = Collections.singletonList(SlidesScopes.PRESENTATIONS_READONLY);
+	private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+	/**
+	 * Creates an authorized Credential object.
+	 * 
+	 * @param HTTP_TRANSPORT The network HTTP Transport.
+	 * @return An authorized Credential object.
+	 * @throws IOException If the credentials.json file cannot be found.
+	 */
+
 	@RequestMapping(value = "/GenerateGoogleSlide", method = RequestMethod.GET)
 	public String generate(HttpSession session) {
 		// remove value from session
-		 Resource resource = new ClassPathResource("classpath:google.json");
-		 
-	        try {
-	        	InputStream inputStream = resource.getInputStream();
-	            byte[] bdata = FileCopyUtils.copyToByteArray(inputStream);
-	            String data = new String(bdata, StandardCharsets.UTF_8);
-	            mLog.info(data);
-	        } catch (Exception e) {
-	        	mLog.severe(e.getMessage());
+
+		try {
+			File file = ResourceUtils.getFile("classpath:google.json");
+			InputStream inputStream = new FileInputStream(file);
+			byte[] bdata = FileCopyUtils.copyToByteArray(inputStream);
+			// Build a new authorized API client service.
+	        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+	        Slides service = new Slides.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+	                .setApplicationName(APPLICATION_NAME)
+	                .build();
+	        //https://docs.google.com/presentation/d/1b4cxJuF4KQJTwQHq41RynOO5EeKhTpCxkRU1MIPXmJ4/edit#slide=id.p1
+	        String presentationId = "1b4cxJuF4KQJTwQHq41RynOO5EeKhTpCxkRU1MIPXmJ4";
+	        Presentation response = service.presentations().get(presentationId).execute();
+	        List<Page> slides = response.getSlides();
+
+	        System.out.printf("The presentation contains %s slides:\n", slides.size());
+	        for (int i = 0; i < slides.size(); ++i) {
+	            System.out.printf("- Slide #%s contains %s elements.\n", i + 1, slides.get(i).getPageElements().size());
 	        }
-		 // ignore id
-		String nextPage = "redirect:/GenerateGoogleSlidePage";
+			//Credential Credential = 
+			String data = new String(bdata, StandardCharsets.UTF_8);
+			mLog.info(data);
+		} catch (Exception e) {
+			mLog.severe(e.getMessage());
+		}
+		// ignore id
+		String nextPage = "googleslidepage";
 		return nextPage;
 
 	}
+	
+	
+	
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private  Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws Exception {
+        // Load client secrets.
+    	File file = ResourceUtils.getFile("classpath:google.json");
+		InputStream inputStream = new FileInputStream(file);
+        InputStream in = new FileInputStream(file);
+       
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        
+        
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
 
 }
